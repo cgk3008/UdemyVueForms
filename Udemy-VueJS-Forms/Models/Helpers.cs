ae1143eb71af1b1a75e75fba48d8ab1cffd35679
Ckspiderman outlook.com <Ckspiderman@outlook.com>
2018-12-28 15:36:01 -0500

    Ok setup this app for the forms. on to part 7

---
+++ Udemy-VueJS-Forms/Models/Helpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Udemy_VueJS_Forms.Models
+{
+    public class Helpers
+    {
+        public static IHtmlString LoadFile(string webpath)
+        {
+            HtmlString returnContent = new HtmlString("");
+            if (!string.IsNullOrWhiteSpace(webpath))
+            {
+                string filepath = System.Web.Hosting.HostingEnvironment.MapPath(webpath);
+                if (File.Exists(filepath))
+                {
+
+                    string content = File.ReadAllText(filepath);
+
+                    if (!string.IsNullOrWhiteSpace(content))
+                    {
+                        returnContent = new HtmlString(content);
+                    }
+                }
+            }
+            return returnContent;
+        }
+    }
+}
